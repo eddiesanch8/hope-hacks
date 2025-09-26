5412ce73dd7db5e378020cf810bd32b8635fea6b
Jane Le <janehtle@gmail.com>
2025-09-26 17:04:21 -0400

    completed landing page, installed hbs npm package, and updated server.js to enable use of hbs

--- server.js
+++ server.js
@@ -7,18 +7,26 @@ const cors = require("cors");
 app.use(cors());
 const PORT = 3000;
 const mysql = require("mysql2/promise");
+const hbs = require("hbs"); //JANE DID IT!!!!
 
 // --------------------- server settings --------------------- \\
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
 // setting up pathing for ejs so it knows where views lives...
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
+//register partials
+hbs.registerPartials(path.join(__dirname, 'views/partials')); //JANE DID IT!!!!
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    intro: "Welcome to ByteSized!",
+    message: "Your new favorite digital newsletter that connects tech professionals, students, and AI enthusiasts with curated news and research on artificial intelligence, making it easy to stay informed about the latest trends, breakthroughs, and applications."
+  });
 });
 
 app.get("/search", (req, res) => {
