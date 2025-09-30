6a90c753206f7c113131cc8cdfd52e8429a71861
Eduardo <46777616+eddiesanch8@users.noreply.github.com>
2025-09-30 17:49:01 -0400

    added client side js for backend integration
    database integration for the signup page is complete, we are just waiting on the actual search page

--- public/src/login.js
+++ public/src/login.js
@@ -3,43 +3,39 @@ console.log("Login JS Loaded"); // Check if JS is connected
 const loginForm = document.getElementById("loginForm");
 const loginMessage = document.getElementById("loginMessage");
 
-if (loginForm) {
-  loginForm.addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent page reload
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  //   creating a new object from our form inputs
+  const formData = new FormData(e.target);
+  //   turning our form object into js object that can be read
+  const data = Object.fromEntries(formData.entries());
 
-    const email = loginForm.email.value.trim();
-    const password = loginForm.password.value;
+  const { email, password } = data;
 
-    // --- Empty field check ---
-    if (!email || !password) {
-      loginMessage.textContent = "Please fill out both fields.";
-      return;
-    }
+  // --- Empty field check ---
+  if (!email || !password) {
+    loginMessage.textContent = "Please fill out both fields.";
+    return;
+  }
 
-    // --- Email format check ---
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-      loginMessage.textContent = "Enter a valid email address.";
-      return;
-    }
-
-    // --- Password length check ---
-    if (password.length < 8) {
-      loginMessage.textContent = "Password must be at least 8 characters long.";
-      return;
-    }
-
-    // --- Password strength check ---
-    const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/;
-    // 1 uppercase, 1 lowercase, 1 number, 1 special char, at least 8 chars
-    if (!passwordPattern.test(password)) {
-      loginMessage.textContent = "Password must be at least 8 characters and include uppercase, lowercase, a number, and a special character.";
-      return;
-    }
-
-    // --- Success case ---
-    loginMessage.textContent = "Login successful! Welcome back!";
-
-    loginForm.reset();
+  //   sending the js object as JSON
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
   });
-}
+
+  const result = await res.json();
+
+  if (res.ok) {
+    loginMessage.innerHTML = "Login successful! Welcome back!";
+    // stores JWT token and redirects user to the get: /dashboard route
+    localStorage.setItem("access_token", result.access_token);
+    console.log(loginMessage);
+    setTimeout(() => {
+      window.location.href = "/signup";
+    }, 2000);
+  } else {
+    alert(result.error);
+  }
+});
