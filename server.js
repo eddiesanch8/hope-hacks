5acd6f4f5d88f9851e1e4418c07bed3f1e244dd2
eddiesanch8 <146777616+eddiesanch8@users.noreply.github.com>
2025-10-06 20:58:03 -0400

    Update server.js
    Cleaned up code/comments

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const path = require("path");
 const cors = require("cors");
-const hbs = require("hbs"); //JANE DID IT!!!!
+const hbs = require("hbs"); 
 
 // -------------------------------- setting up custom modules -------------------------- \\
 const connectToDatabase = require("./config/db");
 const getArticle = require("./lib/api");
 const authRoutes = require("./routes/auth");
 const authenticateToken = require("./routes/authToken");
-const PORT = process.env.PORT || process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 //------------------------------- server settings --------------------------------------- \\
 
@@ -19,26 +19,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-// for testing purposes please dont uncomment or delete this (EDUARDO DID THIS)
-// app.use(express.static(path.join(__dirname, "test-folder")));
 app.use(express.static(path.join(__dirname, "public")));
 
 // login/signup end points
 app.use("/", authRoutes);
 // instantiate db on local machine
 connectToDatabase();
-// setting up pathing for ejs so it knows where views lives...
+// setting up pathing for hbs so it knows where views lives...
 app.set("view engine", "hbs");
-// change back for non-testing
 app.set("views", path.join(__dirname, "views"));
 
-// please dont uncomment or delete this, it is the set up for my testing routes on test-folder: (EDUARDO DID THIS)
-// app.set("views", path.join(__dirname, "test-folder"));
 
 // ---------------------------------------------routes --------------------------------------- \\
 
 //register partials
-hbs.registerPartials(path.join(__dirname, "views/partials")); //JANE DID IT!!!!
+hbs.registerPartials(path.join(__dirname, "views/partials")); 
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -50,27 +45,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/signup", (req, res) => {
-  res.render("signup"); // Helena
+  res.render("signup"); 
 });
 
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-// this actually where our search page will go
 app.get("/dashboard", (req, res) => {
-  //JANE DID IT!!!
   res.render("dashboard");
 });
 
 app.get("/aboutus", (req, res) => {
-  res.render("aboutus"); // HELENA 
+  res.render("aboutus"); 
 });
 
 // this is where our API fetch will happen, it is our exposed endpoint...
 app.get("/search", authenticateToken, async (req, res) => {
   try {
-    // get form input
+    // get form input, short circuit 
     const searchTerm = req.query.search || "No search term provided";
     // fetching API (check lib/api)
     const data = await getArticle(searchTerm);
